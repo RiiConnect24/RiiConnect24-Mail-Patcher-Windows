700871198a928a9cbc6cb2cd6abf8a6cb72428b4
KcrPL <kcrplkcrpl@gmail.com>
2018-01-14 02:26:16 +0100

    Update

--- patch.bat
+++ patch.bat
@@ -1,16 +1,18 @@
+echo %~d0%~p0
+cd "%~d0%~p0"
 :startup_begin
 @echo off
 if exist temp.bat del /q temp.bat
 :: ===========================================================================
 :: Wii Mail Patcher for Windows
-set version=1.0.6
+set version=1.0.9
 :: AUTHORS: KcrPL, Spotlight
 :: ***************************************************************************
 :: Copyright (c) 2017 RiiConnect24, and it's (Lead) Developers
 :: ===========================================================================
 title RiiConnect24 Mail Patcher.
-set last_build=2017/10/28
-set at=18:27
+set last_build=2018/01/14
+set at=1:55
 
 set mode=126,36
 mode %mode%
@@ -27,7 +29,7 @@ set TempStorage=%appdata%\Mail-Patcher\internet\temp
 if %os%=="" goto not_windows_nt
 if not %os%==Windows_NT goto not_windows_nt
 
-if exist %MainFolder%\requirerestart.txt goto reqrestart
+if exist "%MainFolder%\requirerestart.txt" goto reqrestart
 
 set /a patherror=0
 if "%cd%"=="%windir%\system32" set /a patherror=1
@@ -36,31 +38,24 @@ if %patherror%==0 if not exist patch.bat set /a patherror=2
 goto begin_main
 :not_windows_nt
 cls
-echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version%. (Compiled on %last_build% at %at%)
+echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version% (Compiled on %last_build% at %at%)
 echo.
 echo Please don't run our Mail Patcher in MS-DOS :P.
 echo Run it only on Windows Vista+ computer. :)
 pause>NUL	
 exit
 :reqrestart
-del /q %MainFolder%\requirerestart.txt
+del /q "%MainFolder%\requirerestart.txt"
 cls
-echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version%. (Compiled on %last_build% at %at%)
+echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version% (Compiled on %last_build% at %at%)
 echo.
 echo We will now continue the Ruby installation.
 echo Please wait...
 
-echo 4/4 Installing gem.
-call gem install bindata
-echo.
-echo Done! Please wait few seconds.
-timeout 5 /nobreak >NUL
-goto startup_begin
-
 :begin_main
 mode %mode%
 cls
-echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version%. (Compiled on %last_build% at %at%)
+echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version% (Compiled on %last_build% at %at%)
 if %patherror%==0 echo              `..````                                                  
 if %patherror%==0 echo              yNNNNNNNNMNNmmmmdddhhhyyyysssooo+++/:--.`                
 if %patherror%==0 echo              ddmNNd:dNMMMMNMMMMMMMMMMMMMMMMMMMMMMMMMMs                
@@ -229,13 +224,13 @@ set /a updateavailable=0
 :: Update script.
 set updateversion=0.0.0
 :: Delete version.txt and whatsnew.txt
-if %offlinestorage%==0 if exist %TempStorage%\version.txt del %TempStorage%\version.txt /q
-if %offlinestorage%==0 if exist %TempStorage%\whatsnew.txt del %TempStorage%\whatsnew.txt /q
+if %offlinestorage%==0 if exist "%TempStorage%\version.txt" del "%TempStorage%\version.txt" /q
+if %offlinestorage%==0 if exist "%TempStorage%\whatsnew.txt" del "%TempStorage%\whatsnew.txt" /q
 
 if not exist %TempStorage% md %TempStorage%
 :: Commands to download files from server.
-if %Update_Activate%==1 if %offlinestorage%==0 powershell -command "(new-object System.Net.WebClient).DownloadFile('%FilesHostedOn%/whatsnew.txt', '%TempStorage%/whatsnew.txt')"
-if %Update_Activate%==1 if %offlinestorage%==0 powershell -command "(new-object System.Net.WebClient).DownloadFile('%FilesHostedOn%/version.txt', '%TempStorage%/version.txt')"
+if %Update_Activate%==1 if %offlinestorage%==0 powershell -command "(new-object System.Net.WebClient).DownloadFile('"%FilesHostedOn%/whatsnew.txt"', '"%TempStorage%/whatsnew.txt"')"
+if %Update_Activate%==1 if %offlinestorage%==0 powershell -command "(new-object System.Net.WebClient).DownloadFile('"%FilesHostedOn%/version.txt"', '"%TempStorage%/version.txt"')"
 
 	set /a temperrorlev=%errorlevel%
 	
@@ -245,9 +240,9 @@ if %Update_Activate%==1 if not %errorlevel%==0 goto error_update_not_available
 	
 
 :: Copy the content of version.txt to variable.
-if exist %TempStorage%\version.txt set /p updateversion=<%TempStorage%\version.txt
-if not exist %TempStorage%\version.txt set /a updateavailable=0
-if %Update_Activate%==1 if exist %TempStorage%\version.txt set /a updateavailable=1
+if exist "%TempStorage%\version.txt" set /p updateversion=<"%TempStorage%\version.txt"
+if not exist "%TempStorage%\version.txt" set /a updateavailable=0
+if %Update_Activate%==1 if exist "%TempStorage%\version.txt" set /a updateavailable=1
 :: If version.txt doesn't match the version variable stored in this batch file, it means that update is available.
 if %Update_Activate%==1 if %updateversion%==%version% set /a updateavailable=0 
 if %Update_Activate%==1 if %updateavailable%==1 goto update_notice
@@ -373,12 +368,12 @@ set rubyavailable=0
 set rubyversion=INCORECT
 
 ::
-ruby -v && set /a rubyavailable=1
+call ruby -v && set /a rubyavailable=1
 ::
-if exist %TempStorage%\rubyversion.txt del %TempStorage%\rubyversion.txt /q
-ruby -v >>%TempStorage%\rubyversion.txt
+if exist "%TempStorage%\rubyversion.txt" del "%TempStorage%\rubyversion.txt" /q
+call ruby -v >>"%TempStorage%\rubyversion.txt"
 
-if exist %TempStorage%\rubyversion.txt findstr /c:"ruby 2.4.1" "%TempStorage%\rubyversion.txt"
+if exist "%TempStorage%\rubyversion.txt" findstr /c:"ruby 2.5.0p0" "%TempStorage%\rubyversion.txt"
 set temperrorlev=%errorlevel%
 if %errorlevel%==0 set rubyversion=OK
 
@@ -461,7 +456,7 @@ echo                                   -odhhhhyddmmmmmNNmhs/:`
 echo                                     :syhdyyyyso+/-`
 set /p s=
 if %s%==1 goto update_files
-if %s%==2 goto begin_main
+if %s%==2 goto startup_script_files_check
 if %s%==3 goto whatsnew
 goto update_notice
 :update_files
@@ -504,21 +499,22 @@ echo                                     :syhdyyyyso+/-`
 if exist mailparse.rb` del mailparse.rb` /q 2> nul
 
 :: Downloading the update files. In future i'm gonna add something called "Files Version" (at least i call it that way). Because most of the time the patch.bat is only updated
-powershell -command "(new-object System.Net.WebClient).DownloadFile('%FilesHostedOn%/mailparse.rb', 'mailparse.rb`')"
-powershell -command "(new-object System.Net.WebClient).DownloadFile('%FilesHostedOn%/patch.bat', 'patch.bat`')"
+powershell -command "(new-object System.Net.WebClient).DownloadFile('"%FilesHostedOn%/mailparse.rb"', '"mailparse.rb"`')"
+powershell -command "(new-object System.Net.WebClient).DownloadFile('"%FilesHostedOn%/patch.bat"', '"patch.bat"`')"
 
 :: If download failed
 if %update%==1 if not exist patch.bat` goto error_update_not_available
 if %update%==1 if not exist mailparse.rb` goto error_update_not_available
 
 :: Delete the original files
-if %update%==1 if exist mailparse del mailparse.rb /q
+if %update%==1 if exist mailparse.rb del mailparse.rb /q
 
 :: Renaming the temp files to original names
 ren mailparse.rb` mailparse.rb
 
 :: Patch.bat cannot be overwritten while running so i'm creating a small script
-echo ping localhost -n 2 >>temp.bat
+echo echo off >>temp.bat
+echo ping localhost -n 2^>NUL >>temp.bat
 echo del patch.bat /q >>temp.bat
 echo ren patch.bat` patch.bat >>temp.bat
 echo start patch.bat >>temp.bat
@@ -530,18 +526,18 @@ exit
 exit
 :whatsnew
 cls
-if not exist %TempStorage%\whatsnew.txt goto whatsnew_notexist
-echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version%. (Compiled on %last_build% at %at%)
+if not exist "%TempStorage%\whatsnew.txt" goto whatsnew_notexist
+echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version% (Compiled on %last_build% at %at%)
 echo ------------------------------------------------------------------------------------------------------------------------------              
 echo.
 echo What's new in update %updateversion%?
 echo.
-type %TempStorage%\whatsnew.txt
+type "%TempStorage%\whatsnew.txt"
 pause>NUL
 goto update_notice
 :whatsnew_notexist
 cls
-echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version%. (Compiled on %last_build% at %at%)
+echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version% (Compiled on %last_build% at %at%)
 echo -----------------------------------------------------------------------------------------------------------------------------              
 echo.
 echo Error. What's new file is not available.
@@ -596,7 +592,7 @@ if %rubyavailable%==0 set rubyavailablemessage=No
 if %rubyversion%==INCORECT set rubyversionmessage=No
 if %rubyversion%==OK set rubyversionmessage=Yes
 cls
-echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version%. (Compiled on %last_build% at %at%)
+echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version% (Compiled on %last_build% at %at%)
 echo ------------------------------------------------------------------------------------------------------------------------------ 
 echo.
 echo This patcher is for patching nwc24msg.cfg file in order to make RiiConnect24 Mail work on your Wii!
@@ -619,7 +615,7 @@ goto 2_patch_script
 :2_script_error
 cls
 
-echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version%. (Compiled on %last_build% at %at%)
+echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version% (Compiled on %last_build% at %at%)
 echo ------------------------------------------------------------------------------------------------------------------------------ 
 echo.
 echo SOFTWARE FAILRUE
@@ -628,7 +624,7 @@ pause>NUL
 goto startup_script
 :2_download_ruby
 cls
-echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version%. (Compiled on %last_build% at %at%)
+echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version% (Compiled on %last_build% at %at%)
 echo ------------------------------------------------------------------------------------------------------------------------------ 
 echo.
 echo Now, we will fix the problem with ruby version.
@@ -680,7 +676,7 @@ pause>NUL
 goto begin_main
 :2_downloading_ruby
 cls
-echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version%. (Compiled on %last_build% at %at%)
+echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version% (Compiled on %last_build% at %at%)
 echo ------------------------------------------------------------------------------------------------------------------------------ 
 echo.
 echo Downloading and installing ruby.
@@ -690,32 +686,41 @@ echo 1/4 Checking your CPU architecture...
 echo Done!: %processor_architecture%
 echo.
 echo 2/4 Downloading ruby installer!...
-if exist %TempStorage%\rubyinstaller-2.4.1-2-x86.exe del /q %TempStorage%\rubyinstaller-2.4.1-2-x86.exe
-if exist %TempStorage%\rubyinstaller-2.4.1-2-x64.exe del /q %TempStorage%\rubyinstaller-2.4.1-2-x64.exe
-if %processor_architecture%==x86 powershell -command "(new-object System.Net.WebClient).DownloadFile('%FilesHostedOn%/rubyinstaller-2.4.1-2-x86.exe', '%TempStorage%\rubyinstaller-2.4.1-2-x86.exe')"
-if %processor_architecture%==AMD64 powershell -command "(new-object System.Net.WebClient).DownloadFile('%FilesHostedOn%/rubyinstaller-2.4.1-2-x64.exe', '%TempStorage%\rubyinstaller-2.4.1-2-x64.exe')" 
+if exist "%TempStorage%\rubyinstaller-2.5.0-1-x86.exe" del /q "%TempStorage%\rubyinstaller-2.5.0-1-x86.exe"
+if exist "%TempStorage%\rubyinstaller-2.5.0-1-x64.exe" del /q "%TempStorage%\rubyinstaller-2.5.0-1-x64.exe"
+if %processor_architecture%==x86 powershell -command "(new-object System.Net.WebClient).DownloadFile('"%FilesHostedOn%/rubyinstaller-2.5.0-1-x86.exe"', '%TempStorage%\rubyinstaller-2.5.0-1-x86.exe"')"
+if %processor_architecture%==AMD64 powershell -command "(new-object System.Net.WebClient).DownloadFile('"%FilesHostedOn%/rubyinstaller-2.5.0-1-x64.exe"', '%TempStorage%\rubyinstaller-2.5.0-1-x64.exe"')" 
 set actionerrordeb=Downloading Ruby
 if not %errorlevel%==0 goto error_download
 echo Done!
 echo.
 echo 3/4 Installing Ruby in background. This can take a moment or two.
-if %processor_architecture%==x86 call "%TempStorage%\rubyinstaller-2.4.1-2-x86.exe" /verysilent /dir="%appdata%\Ruby24" /tasks="assocfiles,modpath"
-if %processor_architecture%==AMD64 call "%TempStorage%\rubyinstaller-2.4.1-2-x64.exe" /verysilent /dir="%appdata%\Ruby24" /tasks="assocfiles,modpath"
+if %processor_architecture%==x86 call "%TempStorage%\rubyinstaller-2.5.0-1-x86.exe" /verysilent /dir="%appdata%\Ruby25" /tasks="assocfiles,modpath"
+if %processor_architecture%==AMD64 call "%TempStorage%\rubyinstaller-2.5.0-1-x64.exe" /verysilent /dir="%appdata%\Ruby25" /tasks="assocfiles,modpath"
 set actionerrordeb=Installing Ruby.
 if not %errorlevel%==0 goto error_download
-echo Done!
+echo Refreshing PATH from registry...
+for /f "tokens=3*" %%A in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path') do set syspath=%%A%%B
+for /f "tokens=3*" %%A in ('reg query "HKCU\Environment" /v Path') do set userpath=%%A%%B
+set PATH=%userpath%;%syspath%
+echo 4/4 Installing gem.
+call gem install bindata
+if not %errorlevel%==0 goto error_download
+set actionerrordeb=Installing gem
 echo.
-echo Press any button and the patcher will exit. Please restart it.
-timeout 2 /nobreak >NUL
+echo Done! Patching will start in 5 seconds.
+timeout 5 /nobreak >NUL
+goto 2_patch_script
+
 pause
 goto after_install_restart
 :after_install_restart
 cls
-echo .>>%MainFolder%\requirerestart.txt
+echo .>>"%MainFolder%\requirerestart.txt"
 exit
 :2_patch_script
 cls
-echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version%. (Compiled on %last_build% at %at%)
+echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version% (Compiled on %last_build% at %at%)
 echo ------------------------------------------------------------------------------------------------------------------------------ 
 echo.
 echo In order to patch Mail configuration file, I need that file.
@@ -733,7 +738,7 @@ ping localhost -n 3 >NUL
 goto 2_patch_script
 :error_patching
 cls
-echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version%. (Compiled on %last_build% at %at%)
+echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version% (Compiled on %last_build% at %at%)
 mode %mode%
 cls
 echo.                                                                       
@@ -772,14 +777,17 @@ echo                                     :syhdyyyyso+/-`
 pause>NUL
 goto begin_main
 :3
+if not exist backup-configuration md backup-configuration
+copy "nwc24msg.cfg" "backup-configuration\nwc24msg_%date%.cfg" /y
+set /a temperrorlev=0
 cls
-echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version%. (Compiled on %last_build% at %at%)
+echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version% (Compiled on %last_build% at %at%)
 echo ------------------------------------------------------------------------------------------------------------------------------ 
 echo.
 echo Patching nwc24msg.cfg...
 set actionerrordeb=Patching
-start mailparse.rb
-set temperrorlev=%errorlevel%
+ruby mailparse.rb
+set /a temperrorlev=%errorlevel%
 
 if %temperrorlev%==0 goto end
 
@@ -802,7 +810,7 @@ goto end1
 mode %mode%
 cls
 echo.
-echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version%. (Compiled on %last_build% at %at%)
+echo RiiConnect24 Mail Patcher - (C) KcrPL, (C) Spotlight v%version% (Compiled on %last_build% at %at%)
 echo ------------------------------------------------------------------------------------------------------------------------------ 
 echo  [*] Thanks for using the Patcher! :)
 echo.
